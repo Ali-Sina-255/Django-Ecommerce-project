70ad121dd9f190ac7af2ce8db7c4bc5988d05051
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-04-11 16:20:37 +0430

    cart app functionality is finished

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'category.apps.CategoryConfig',
     'accounts.apps.AccountsConfig',
     'store.apps.StoreConfig',
+    'carts.apps.CartsConfig',
     
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('greatkart.urls')),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
